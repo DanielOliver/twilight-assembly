e979ddc23332ac494fdb4df6b48241ce025b7171
Daniel <danieloliversoftware@gmail.com>
2022-07-19 21:06:03 -0400

    node-version 18.x

--- ti4-static/pages/index.tsx
+++ ti4-static/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
 import { Container } from 'react-bootstrap'
 import Layout from '../components/layout'
 
@@ -9,6 +10,19 @@ const Home: NextPage = () => {
       <Container>
         <p>I strongly hope and expect there will be something here someday.</p>
       </Container>
+
+      <footer >
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
     </>
   )
 }
