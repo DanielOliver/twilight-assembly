81a5ae0e6fc78516664699d28761318ab23a5f74
Daniel <danieloliversoftware@gmail.com>
2023-05-26 10:56:37 -0400

    ts-ignore window

--- packages/ui/useWindowSize.ts
+++ packages/ui/useWindowSize.ts
@@ -13,15 +13,19 @@ function useWindowSize() {
     function handleResize() {
       // Set window width/height to state
       setWindowSize({
+        //@ts-ignore
         width: window.innerWidth,
+        //@ts-ignore
         height: window.innerHeight,
       });
     }
     // Add event listener
+    //@ts-ignore
     window.addEventListener("resize", handleResize);
     // Call handler right away so state gets updated with initial window size
     handleResize();
     // Remove event listener on cleanup
+    //@ts-ignore
     return () => window.removeEventListener("resize", handleResize);
   }, []); // Empty array ensures that effect is only run on mount
   return windowSize;
